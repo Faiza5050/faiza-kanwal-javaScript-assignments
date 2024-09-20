b5ee1bea694b6e74a0046317e856d466e47f038a
FAIZA KANWAL <155901304+Faiza5050@users.noreply.github.com>
2024-09-21 01:44:02 +0500

    Add files via upload

---
+++ JavaScript/jscript-assignment5/chapter-12-13/app.js
@@ -0,0 +1,111 @@
+// // Question # 1:
+
+// var checkCharactor = input;
+// var charCode = input.charCodeAt(0);
+// if (typeof input !== "string" || input.length !== 1) {
+//     console.log("Please provide a single character.");
+// } else if (charCode >= 48 && charCode <= 57) {
+//     console.log("The input is a number.");
+// } else if (charCode >= 65 && charCode <= 90) {
+//     console.log("The input is an uppercase letter.");
+// } else if (charCode >= 97 && charCode <= 122) {
+//     console.log("The input is a lowercase letter.");
+// } else {
+//     console.log("The input is neither a number nor an uppercase or lowercase letter.");
+// }
+
+
+// // Question # 2:
+
+// var integers = prompt("num1, num2"); 
+// var num1 = number;
+// var num2 = number;
+
+//     // Compare the integers
+//      if (num1 > num2) {
+//         console.log("The larger integer is: " + num1);
+//     } else if (num1 < num2) {
+//         console.log("The larger integer is: " + num2);
+//     } else if (num1 === num2) {
+//         console.log("Integers are equal.");
+//     } else {
+//         console.log("Enter inputs must be integers.");
+//     }
+
+
+// // Question # 3:
+
+// var number = prompt("Please enter a number:");
+ 
+//  if (number > "0") {
+//     console.log("The number is positive.");
+// } else if (number < "0") {
+//     console.log("The number is negative.");
+// } else if (number === "0") {
+//     console.log("The number is zero.");
+// } else {
+//     console.log("Please enter valid number. Try again");
+// }
+
+
+// // Question # 4:
+
+// var vowels = prompt("Please enter a character:");
+// var vowels = "aeiou";
+
+// if (vowels.length === 1) {
+//     console.log("The character is a vowel.");
+// } else if (vowels === aeiou) {
+//     console.log("True: The character is a vowel.");
+// } else if (vowels != aeiou) {
+//     console.log("False: The character is not a vowel.");
+// } else {
+//     console.log("Please enter exactly one character.");
+// }
+
+
+// // Question # 5:
+
+// // (Part a)
+// var correctPassword = "pass123";
+// // (Part b)
+// var password = prompt("Please enter your password: pass123");
+
+// // (Part c)
+// if (password === null) {
+//     console.log("Please enter your Password.");
+// } else if (password === correctPassword) {
+//     console.log("Correct! The Password you entered matches the original password.");
+// } else {
+//     console.log("Incorrect password.");
+// }
+
+
+// // Question # 6:
+
+// var greeting = prompt("Please Enter hour:");
+// var hour = 13;
+
+// if (hour < 18) {
+//     greeting = "Good day";
+// } else {
+//     greeting = "Good evening";
+// }
+// console.log(greeting);
+
+
+// // Question # 7:
+
+// var time = prompt("Please enter the time in 24-hour format (e.g., 19:00):");
+
+// if (time >= 0 && time < 12) {
+//     console.log("Good Morning");
+// } else if (time >= 12 && time < 17) {
+//     console.log("Good Afternoon");
+// } else if (time >= 17 && time < 21) {
+//     console.log("Good Evening");
+// } else if (time >= 21 && time < 24) {
+//     console.log("Good Night");
+// } else {
+//     console.log("Please enter hours between 24-hour.");
+// }
---
+++ JavaScript/jscript-assignment5/chapter-14-16/app.js
@@ -0,0 +1,129 @@
+// // Question # 1,2:
+
+// var studentNames = [];
+
+
+// // Question # 3:
+
+// var studentNames = ["faiza", "maha", "areesha", "shereen"];
+
+
+// // Question # 4:
+
+// var arrNumbers = [5, 10, 15, 20];
+
+
+// // Question # 5:
+
+// var boolArray = [true, false];
+
+
+// // Question # 6:
+
+// var mixedArray = ["karachi", 30, true, "Islamabad", 45, false, null];
+
+
+// // Question # 7:
+
+// var qualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil.", "PhD"];
+// document.write("<h1>Qualifications:</h1>");
+
+// document.write("<h3><ol>");
+// document.write("<li>" + qualifications[0] + "</li>");
+// document.write("<li>" + qualifications[1] + "</li>");
+// document.write("<li>" + qualifications[2] + "</li>");
+// document.write("<li>" + qualifications[3] + "</li>");
+// document.write("<li>" + qualifications[4] + "</li>");
+// document.write("<li>" + qualifications[5] + "</li>");
+// document.write("<li>" + qualifications[6] + "</li>");
+// document.write("<li>" + qualifications[7] + "</li>");
+// document.write("</ol></h3>");
+
+
+// // Question # 8:
+
+// var studentNames = ["Michael", "John", "Tony"];
+// var scores = [320, 230, 480];
+// totalMarks = 500;
+
+// for (let i = 0; i < studentNames.length; i++) {
+//     var percentage = (scores[i] / totalMarks) * 100;
+// document.write("Score of " + studentNames[i] + " is " + scores[i] + ". Percentage: " + percentage + " % <br>");
+// }
+
+
+// // Question # 9:
+
+// var colors = [" purple ", " green ", " yellow ", " red "];
+// document.write("<br>" + colors);
+
+// // // (Part a)
+// var userColor = prompt("What color would you like to add to the beginning of the array?");
+// colors.unshift(userColor);
+// document.write("<br>" + colors + " ");
+
+// // // (Part b)
+// var userColor = prompt("What color would you like to add to the end of the array?");
+// colors.push(userColor);
+// document.write("<br>" + " " + colors);
+
+// // (Part c)
+// var colors = [" blue ", " brown ", " purple ", " green ", " yellow ", " red "];
+// document.write("<br>" + colors);
+
+// // (Part d)
+// var colors = [" blue ", " brown ", " purple ", " green ", " yellow ", " red "];
+// colors.shift(userColor);
+// document.write("<br>" + colors + " ");
+
+// // (Part e)
+// var colors = [" blue ", " brown ", " purple ", " green ", " yellow ", " red "];
+// colors.pop(userColor);
+// document.write("<br>" + colors + " ");
+
+// // (Part f)
+// var colors = [" blue ", " brown ", " purple ", " green ", " yellow ", " red "];
+// var index = prompt("At which index (0 to " + (colors.length) + ") would you like to add a color?");
+// var colorName = prompt("What color would you like to add?");
+
+// colors.splice(index, 1, colorName);
+// document.write("<br>" + colors);
+
+// // (Part g)
+// var index1 = prompt("At which index (0 to " + (colors.length) + ") would you like to delete color(s)?");
+// var countColors = prompt("How many colors would you like to delete?");
+
+// colors.splice(index1, countColors);
+// document.write("<br>" + colors);
+
+
+// // Question # 10:
+
+// var studScore = [320, 230, 480, 120];
+// document.write("Scores of Students : " + studScore);
+
+// // studScore.sort(studScore);
+// document.write("<br> Ordered Scores of Students : " + studScore.sort());
+
+
+// // Question # 11:
+
+// var cityNames = ["Karachi", "Lahore", "islamabad", "quetta", "Peshawar"];
+// document.write("Cities list: <br> " + cityNames + "<br>");
+
+// var citiesSelect = cityNames.slice(2, 4);
+// document.write("<br> Selected cities list: <br> " + citiesSelect);
+
+
+// // Question # 12:
+
+// var arr = ["This ", "is ", "my ", "cat"];
+// document.write("<h1>Array: <br> This,is,my,cat</h1>");
+
+// var joining = arr.join("");
+// document.write("<h1>String: <br>" + joining + "</h1>");
+
+
+// Question # 13:
+
+
